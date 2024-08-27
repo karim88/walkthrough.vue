896e5c2b75ce32c18d1a3c5c53ef440fdb6e1b54
Karim Oulad Chalha <ouladchalha.k@gmail.com>
2024-08-27 23:51:17 +0200

    feat: editing vite config to build vuejs files

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,58 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'exclude-svg-imports',
+      enforce: 'pre',
+      resolveId(source) {
+        console.log(source)
+        if (source.endsWith('.svg')) {
+          return source;
+        }
+      },
+      load(id) {
+        if (id.endsWith('.svg')) {
+          throw new Error(`SVG files are not allowed: ${id}`);
+        }
+      },
+    },
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   build: {
     lib: {
-      entry: './src/plugins/walkthrough.ts',
+      entry: resolve(__dirname, 'src/plugins/walkthrough.ts'), // Adjust the path to your entry file
       name: 'VueWalkthrough',
-      fileName: (format) => `vue-walkthrough.${format}.js`
+      fileName: (format) => `vue-walkthrough.${format}.js`,
     },
     rollupOptions: {
+      // Ensure to externalize dependencies that you do not want to bundle into your library
       external: ['vue'],
       output: {
+        // Provide global variables to use in the UMD build for externalized deps
         globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
-})
+          vue: 'Vue',
+        },
+      },
+      plugins: [
+        {
+          name: 'exclude-assets',
+          resolveId(source) {
+            if (/\.(png|jpe?g|gif|svg)$/.test(source)) {
+              return { id: source, external: true };
+            }
+            return null;
+          },
+        },
+      ],
+    },
+  },
+});

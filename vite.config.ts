ea8f13dc9dbc90e3e5809037d965168645708279
Karim Oulad Chalha <ouladchalha.k@gmail.com>
2024-08-27 23:03:23 +0200

    feat: adding build instructions in vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: './src/plugins/walkthrough.ts',
+      name: 'VueWalkthrough',
+      fileName: (format) => `vue-walkthrough.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
